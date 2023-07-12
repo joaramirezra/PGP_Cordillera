673435b14075b3ef81a9c7ca458f38eaa652736f
joaramirezra <joaramirezra@unal.edu.co>
2023-07-12 07:04:56 -0500

    V0.1.9 despliege

--- app.py
+++ app.py
@@ -20,37 +20,23 @@ def index():
 def map_selection():
     if request.method == 'POST':
         map_selection = request.form['map']
-        map_center = None
 
-        if map_selection == 'map1':
-            map_center = [51.5074, -0.1278]  # London, UK
-        elif map_selection == 'map2':
-            map_center = [35.8617, 104.1954]  # China
-        elif map_selection == 'map3':
-            map_center = [4.7110, -74.0721]  # Colombia
+        if map_selection not in ['map1', 'map2']:
+            # Handle invalid selection, maybe return an error message
+            return "Invalid map selection", 400
 
-        if map_center is not None:
-            m = folium.Map(location=map_center, zoom_start=5)
-            folium.Marker(location=map_center, popup='Selected Location').add_to(m)
-
-            # Generate a unique filename for the map
-            map_filename = 'map_{}.html'.format(uuid.uuid4())
-            map_file_path = os.path.join('static', map_filename)
-
-            # Save the map to a file
-            m.save(map_file_path)
-
-            # Redirect to the map processing page
-            return redirect(url_for('process_map', map_filename=map_filename, map_selection=map_selection))
+        # Redirect to the map processing page with the map selection as an argument
+        return redirect(url_for('process_map'))
 
     return render_template('map.html')
 
+
 @app.route('/process_map', methods=['GET'])
 def process_map():
-    map_filename = request.args.get('map_filename')
     map_selection = request.args.get('map_selection')
 
-    return render_template('process_map.html', map_filename=map_filename, map_selection=map_selection)
+    return render_template('process_map.html')
+
 
 
 if __name__ == '__main__':
